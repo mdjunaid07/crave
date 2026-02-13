fb804223ee22cb8140b75b1981e51534ad5b93ca
junaid <itsaboycse@gmail.com>
2026-02-13 20:03:24 +0530

    security enabled

--- script.js
+++ script.js
@@ -67,3 +67,15 @@ document.addEventListener('DOMContentLoaded', function() {
     document.removeEventListener("touchmove", drag);
   }
 
+
+
+  document.addEventListener("contextmenu", function(e) {
+  e.preventDefault();
+});
+
+document.onkeydown = function(e) {
+  if (e.keyCode == 123) return false; // F12
+  if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) return false;
+  if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) return false;
+  if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) return false;
+};
